bc247b8b2ab35d508aca58d7c4d791d1290a5ddd
whuang214 <whuang2146@gmail.com>
2023-04-17 19:24:05 -0400

    isolated randomv
    TODO: fix hash collision

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        
+        HashMap<Integer, String> index = HashBasedIndex.buildIndex();
+        System.out.println(index.size());
+
     }
+
 }
+
