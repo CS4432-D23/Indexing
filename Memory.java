a3bd7ffc2ecd95096dc88c5f7c0115e1fd7c1003
Will Huang <whuang2146@gmail.com>
2023-04-17 23:23:01 -0400

    added section 2 and 3

---
+++ Memory.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * This class is used to build the index for the Project2Dataset directory.
+ */
+public class Memory {
+
+    private static Memory instance; // the instance of the Memory class
+    private HashMap<Integer, ArrayList<String>> hashIndex = null;
+    private String[] arrayIndex = null;
+
+    /*
+     * This is a private constructor to prevent the creation of new instances of the
+     */
+    private Memory() {
+    }
+
+    /**
+     * This method returns the instance of the Memory class.
+     * @return the instance of the Memory class
+     */
+    public static Memory getInstance() {
+        if (instance == null) {
+            instance = new Memory();
+        }
+        return instance;
+    }
+
+    
+    /**
+     * This method builds a hash-based and array-based index for the Project2Dataset directory.
+     * 
+     * @return a hash map of the index
+     */
+    public void buildIndex() {
+        HashMap<Integer, ArrayList<String>> hashIndex = new HashMap<>();
+        String[] arrayIndex = new String[5000];
+
+
+        File directory = new File("Project2Dataset");
+
+        for (File file : directory.listFiles()) {
+            String fileNumber = file.getName().substring(1, file.getName().lastIndexOf('.'));
+            // System.out.println(fileNumber);
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                String[] recordArray = line.split("\\.\\.\\."); // split the line into records
+                for (String record : recordArray) {
+                    String[] fieldArray = record.split(","); // split the record into fields
+                    String offset = fieldArray[0].substring(fieldArray[0].length() - 3); // extract the offset
+                    // System.out.println(offset);
+                    int RandomV = Integer.parseInt(fieldArray[3].replaceAll("\\s", "")); // extract the RandomV
+        
+
+                    if (hashIndex.containsKey(RandomV)) {
+                        ArrayList<String> fileAndOffsetList = new ArrayList<>();
+                        fileAndOffsetList.add(fileNumber + " | " + offset);
+                        hashIndex.get(RandomV).addAll(fileAndOffsetList);
+
+                    } else {
+                        ArrayList<String> fileAndOffsetList = new ArrayList<>();
+                        fileAndOffsetList.add(fileNumber + " | " + offset);
+                        hashIndex.put(RandomV, fileAndOffsetList);
+                    }
+
+                    if (arrayIndex[RandomV - 1] == null) {
+                        arrayIndex[RandomV - 1] = fileNumber + " | " + offset;
+                    } else {
+                        arrayIndex[RandomV - 1] += ", " + fileNumber + " | " + offset;
+                    }
+
+                }
+                br.close();
+            } catch (Exception e) {
+                System.out.println("Error reading file: " + fileNumber);
+                e.printStackTrace();
+            }
+
+        }
+
+        this.hashIndex = hashIndex;
+        this.arrayIndex = arrayIndex;
+    }
+
+    /**
+     * This method returns the hash-based index.
+     * @return the hash-based index
+     */
+    public HashMap<Integer, ArrayList<String>> getHashIndex() {
+        return hashIndex;
+    }
+
+    /**
+     * This method returns the array-based index.
+     * @return the array-based index
+     */
+    public String[] getArrayIndex() {
+        return arrayIndex;
+    }
+
+
+}
--- main.java
+++ main.java
@@ -1,13 +1,36 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        // System.out.println("Hello World!");
-        
-        HashMap<Integer, String> index = HashBasedIndex.buildIndex();
-        System.out.println(index.size());
+        Scanner sc = new Scanner(System.in);
+        Memory.getInstance();
 
+        while (true) {
+            System.out.println("Program is ready and waiting for user command.");
+            String Command = sc.nextLine();
+
+            // System.out.println(Command);
+
+            String[] commandArray = Command.split(" ");
+            if (commandArray[0].toUpperCase().equals("CREATE")) {
+                Memory.getInstance().buildIndex();
+                // for (int i = 0; i < 1000; i++) {
+                //     System.out.println("RandomV: " + (i + 1) + " File and Offsets: " + Memory.getInstance().getArrayIndex()[i]);
+                // }
+                // for (int i = 0; i < 1000; i++) {
+                //     System.out.println("RandomV: " + (i + 1) + " File and Offsets: " + Memory.getInstance().getHashIndex().get(i + 1));
+                // }
+            } else if (commandArray[0].toUpperCase().equals("SELECT")) {
+
+            } else if (commandArray[0].toUpperCase().equals("EXIT")) {
+                break;
+            }
+
+        }
+
+        sc.close();
     }
 
 }
-
